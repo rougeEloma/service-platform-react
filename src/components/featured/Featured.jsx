fbd68cc667631e58c14fa1641ae23f799c4b569e
Alain ELOMA <alainelomapro@gmail.com>
2024-01-07 21:01:13 +0100

    home featured cpnt started

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,16 @@
+import "./featured.css"
+
+const Featured = () => {
+    return (
+        <>
+        <div>____________</div>
+        <footer className="footer">
+            <div className="categories"> footer category 1 </div>
+            <div className="categories"> footer category 2 </div>
+            <div className="categories"> footer category 3 </div>
+        </footer>
+        </>
+    )
+}
+
+export default Featured;
