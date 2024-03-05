c8c2650328728e6448518512781e6563b13f1366
ALAIN ELOMA <alainelomapro@gmail.com>
2024-03-05 15:05:31 +0100

    trusted-by section - started

---
+++ src/components/TrustedBy/TrustedBy.jsx
@@ -0,0 +1,17 @@
+import "./trusted-by.css"
+
+function TrustedBy() {
+  return (
+    <div className="trusted-by">
+        <div className="trust-text">Approuvé par : </div>
+        <div className="trust-list">
+            <span className="sponsors">kadea</span>
+            <span className="sponsors">controle BTP</span>
+            <span className="sponsors">minister</span>
+            <span className="sponsors">vodacom</span>
+        </div>
+    </div>
+  )
+}
+
+export default TrustedBy
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/TrustedBy/TrustedBy";
 
 const Home = () => {
     return (
         <div>
             <br/><br/><br/>
             <Featured/>
+            <TrustedBy/>
             <section>section 2</section>
             <section>section 3</section>
             
