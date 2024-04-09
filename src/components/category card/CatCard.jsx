3a34abfea7c73bfa0ac2c79539a4fa86ae7679f2
ALAIN ELOMA <alainelomapro@gmail.com>
2024-04-09 15:34:33 +0100

    dummy datas - created

--- src/components/category card/CatCard.jsx
+++ src/components/category card/CatCard.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom"
 import "./catCard.css"
 
 const CatCard = ({item}) => {
   return (
+    <Link to="/services?cat=design">
     <div className="cat-card">
-        <div className="container"></div>
+        <div className="container">
+            <img src={item.img} alt="" />
+            <span className="desc">{item.desc}</span>
+            <span className="title">{item.title}</span>
+        </div>
     </div>
+    </Link>
   )
 }
 
