9490f6594b109a257668b10a5ce5f9f5a87e5231
ALAIN ELOMA <alainelomapro@gmail.com>
2024-04-13 00:07:40 +0100

    service page - started but scss problem

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,46 +1,35 @@
-import "./GigCard.scss";
 import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import newRequest from "../../utils/newRequest";
+import "./GigCard.scss";
 
 const GigCard = ({ item }) => {
-  const { isLoading, error, data } = useQuery({
-    queryKey: [item.userId],
-    queryFn: () =>
-      newRequest.get(`/users/${item.userId}`).then((res) => {
-        return res.data;
-      }),
-  });
+  // const { isLoading, error, data } = useQuery({
+  //   queryKey: [item.userId],
+  //   queryFn: () =>
+  //     newRequest.get(`/users/${item.userId}`).then((res) => {
+  //       return res.data;
+  //     }),
+  // });
   return (
-    <Link to={`/gig/${item._id}`} className="link">
+    <Link to="/gig/123">
       <div className="gigCard">
-        <img src={item.cover} alt="" />
+        <img src={item.img} alt="" />
         <div className="info">
-          {isLoading ? (
-            "loading"
-          ) : error ? (
-            "Something went wrong!"
-          ) : (
-            <div className="user">
-              <img src={data.img || "/img/noavatar.jpg"} alt="" />
-              <span>{data.username}</span>
-            </div>
-          )}
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+          </div>
           <p>{item.desc}</p>
           <div className="star">
             <img src="./img/star.png" alt="" />
-            <span>
-              {!isNaN(item.totalStars / item.starNumber) &&
-                Math.round(item.totalStars / item.starNumber)}
-            </span>
+            <span>{item.star}</span>
           </div>
         </div>
         <hr />
-        <div className="detail">
+        <div className="details">
           <img src="./img/heart.png" alt="" />
           <div className="price">
             <span>À PARTIR DE</span>
-            <h2>$ {item.price}</h2>
+            <h2>${item.price}</h2>
           </div>
         </div>
       </div>
--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,46 +1,56 @@
+
+import { useState } from "react"
 import "./Services.scss"
+import { services } from "../../datas/data"
+import GigCard from "../../components/gigCard/GigCard"
 
 const Services = () => {
+    const [sort, setSort] = useState("sales")
+    const [open, setOpen] = useState(false)
+
+    const reSort = (type) => {
+        setSort(type)
+        setOpen(false)
+    }
+
   return (
     <div className="services">
         <div className="container">
-        <span className="breadcrumbs">LBN | categorie |</span>
-        <h1>Ménage</h1>
-        <p>
-            Explorez les limites de l'art et de la technologie avec les artistes IA de Liverr
-        </p>
-        <div className="menu">
-          <div className="left">
-            <span>Budget</span>
-            <input ref={minRef} type="number" placeholder="min" />
-            <input ref={maxRef} type="number" placeholder="max" />
-            <button onClick={apply}>Appliquer</button>
-          </div>
-          <div className="right">
-            <span className="sortBy">Trier Par</span>
-            <span className="sortType">
-              {sort === "sales" ? "Best Selling" : "Newest"}
-            </span>
-            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
-            {open && (
-              <div className="rightMenu">
-                {sort === "sales" ? (
-                  <span onClick={() => reSort("createdAt")}>Le plus récent</span>
-                ) : (
-                  <span onClick={() => reSort("sales")}>Le plus vendu</span>
-                )}
-                <span onClick={() => reSort("sales")}>Le plus populaire</span>
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="cards">
-          {isLoading
-            ? "loading"
-            : error
-            ? "Something went wrong!"
-            : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
-        </div>
+            <span className="breadcrumbs">LBN | categorie |</span>
+            <h1>Ménage</h1>
+            <p>
+                Explorez les limites de l'art et de la technologie avec les artistes IA de Liverr
+            </p>
+            <div className="menu">
+                <div className="left">
+                    <span>Budget</span>
+                    <input type="number" placeholder="min" />
+                    <input type="number" placeholder="max" />
+                    <button>Appliquer</button>
+                </div>
+                <div className="right">
+                    <span className="sortBy">Trier Par </span>
+                    <span className="sortType">
+                    {sort === "sales" ? "populaire" : "récent"}
+                    </span>
+                    <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+                    {open && (
+                    <div className="rightMenu">
+                        {sort === "sales" ? (
+                        <span onClick={() => reSort("createdAt")}>récent</span>
+                        ) : (
+                        <span onClick={() => reSort("sales")}>populaire</span>
+                        )}
+                        {/* <span onClick={() => reSort("popular")}>populaire</span> */}
+                    </div>
+                    )}
+                </div>
+            </div>
+            <div className="cards">
+                {services.map(service => (
+                    <GigCard key={service.id} item={service} />
+                ))}
+            </div>
         </div>
     </div>
   )
